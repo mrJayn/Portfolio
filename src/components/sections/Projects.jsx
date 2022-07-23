d6af75564145397a12d3a0a16c5ad3fd12982f4a
Michael Jayne <100537512+mrJayn@users.noreply.github.com>
2022-07-23 12:24:03 -0400

    Project Item update

--- src/components/sections/Projects.jsx
+++ src/components/sections/Projects.jsx
@@ -1,56 +1,90 @@
-import Image from 'next/image'
+import { useState } from 'react'
+import { motion, AnimatePresence } from 'framer-motion'
 import { Section } from '@components'
-import { FaGithub } from 'react-icons/fa'
 import data from '@data'
 
+const threshold = 100
+
+const variants = {
+    enter: (direction) => ({
+        x: direction > 0 ? '100%' : '-100%',
+        opacity: 0,
+        transition: { duration: 0.25, ease: 'easeIn' },
+    }),
+    active: {
+        x: 0,
+        opacity: 1,
+        transition: { delay: 0.25 },
+    },
+    exit: (direction) => ({
+        x: direction > 0 ? '-100%' : '100%',
+        opacity: 0,
+        transition: { duration: 0.25, ease: 'easeIn' },
+    }),
+}
+
+const slides = [...Array(data.projects.length).keys()]
+
 const Projects = () => {
+    const [[currentSlide, direction], setCurrentSlide] = useState([0, 0])
+
+    function detectGesture(e, { offset }) {}
+
+    const paginate = (newDirection) => {}
+
     return (
         <Section id="projects" count={false} subsection={true}>
             <h3>Other Projects by Me</h3>
-            <div className="projects-content">
-                {data.projects.map((ap) => (
-                    <div className="project useInView" key={ap.item}>
-                        <div className="gh-link">
-                            <a
-                                href={ap.github}
-                                target="_blank"
-                                rel="noreferrer"
-                            >
-                                <FaGithub size={25} />
-                            </a>
-                        </div>
-                        <a
-                            href="visit-project"
-                            aria-label="Project Link"
-                            target="_blank"
-                            className="pr-wrap"
-                        >
-                            <h4>{ap.title}</h4>
-                            <div>
-                                <div className="pr-tech">
-                                    {ap.technologies.map((item) => {
-                                        return (
-                                            <p className="tech-item" key={item}>
-                                                {item}
-                                            </p>
-                                        )
-                                    })}
-                                </div>
-                                <div className="pr-desc">
-                                    <p>{ap.message}</p>
-                                </div>
-                            </div>
-                        </a>
-                        <Image
-                            src={ap.src}
-                            alt={`${ap.title} Project Image`}
-                            layout="fill"
-                            objectFit="cover"
-                        />
-                    </div>
-                ))}
+            <div className="projects-slider-container">
+                <AnimatePresence custom={direction}>
+                    <motion.div
+                        className="project-slide"
+                        key={currentPage}
+                        data-page={currentPage}
+                        custom={direction}
+                        variants={variants}
+                        initial="enter"
+                        animate="active"
+                        exit="exit"
+                        drag="x"
+                        onDrag={detectPaginationGesture}
+                        onDragEnd={detectGesture}
+                        dragConstraints={{ left: 0, right: 0 }}
+                    ></motion.div>
+                </AnimatePresence>
             </div>
         </Section>
+    )
+}
+
+const SlideSelectors = ({ currentSlide, setPage }) => {
+    return (
+        <AnimateSharedLayout>
+            <div className="Indicators">
+                {pages.map((page) => (
+                    <Indicator
+                        key={page}
+                        onClick={() => setPage(page)}
+                        isSelected={page === currentSlide}
+                    />
+                ))}
+            </div>
+        </AnimateSharedLayout>
+    )
+}
+
+const Indicator = ({ isSelected, onClick }) => {
+    return (
+        <div className="Indicator-container" onClick={onClick}>
+            <div className="Indicator">
+                {isSelected && (
+                    <motion.div
+                        className="Indicator-highlight"
+                        layoutId="highlight"
+                    />
+                )}
+            </div>
+        </div>
     )
 }
 
--- src/components/sections/items/Project_Item.jsx
+++ src/components/sections/items/Project_Item.jsx
@@ -1,7 +1,45 @@
-const Project_Item = () => {
+import data from '@data'
+import Image from 'next/image'
+
+import { FaGithub } from 'react-icons/fa'
+
+const Project_Item = ({ currentSlide }) => {
+    const slide = data.projects[currentSlide]
     return (
-        <div>
-            <div>Hello</div>
+        <div className="project useInView" key={slide.item}>
+            <div className="gh-link">
+                <a href={slide.github} target="_blank" rel="noreferrer">
+                    <FaGithub size={25} />
+                </a>
+            </div>
+            <a
+                href="visit-project"
+                aria-label="Project Link"
+                target="_blank"
+                className="pr-wrap"
+            >
+                <h4>{slide.title}</h4>
+                <div>
+                    <div className="pr-tech">
+                        {slide.technologies.map((item) => {
+                            return (
+                                <p className="tech-item" key={item}>
+                                    {item}
+                                </p>
+                            )
+                        })}
+                    </div>
+                    <div className="pr-desc">
+                        <p>{slide.message}</p>
+                    </div>
+                </div>
+            </a>
+            <Image
+                src={slide.src}
+                alt={`${slide.title} Project Image`}
+                layout="fill"
+                objectFit="cover"
+            />
         </div>
     )
 }
