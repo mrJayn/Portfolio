eda25654755e03b58b28b01c905de4fc4da767f1
mrJayn <100537512+mrJayn@users.noreply.github.com>
2022-11-12 17:41:07 +0000

    Deploying to gh-pages from @ mrJayn/Portfolio@0077a58635d27d4cebe60bd324956358452b2109 🚀

---
+++ index.js
@@ -0,0 +1,7 @@
+const assets = {
+    projects: require('./assets/projects/index.js').default,
+    skills: require('./assets/skills/index.js').default,
+    misc: require('./assets/misc/index.js').default,
+}
+
+export default assets
